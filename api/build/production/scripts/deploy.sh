060c8c58f0992510839774e03919ed26d508462b
Jason Andres <jason@commerce7.com>
2024-08-24 08:06:16 -0700

    Update lambda function

--- api/build/production/scripts/deploy.sh
+++ api/build/production/scripts/deploy.sh
@@ -8,25 +8,16 @@ export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output tex
 # export envars
 export $(egrep -v '^#' .env | xargs)
 
-ECR_SERVICE_NAME=$APP_NAME-ecr
-APP_SERVICE_NAME=$APP_NAME-app
-SQS_SERVICE_NAME=$APP_NAME-sqs
 echo $APP_NAME
+npm install
+mv node_modules dependencies/nodejs/
 
-# Logging in to Amazon ECR
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
-echo Building the Docker image...
-docker build --platform linux/amd64 --build-arg AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID --build-arg AWS_DEFAULT_REGION=us-west-2 -t $ECR_SERVICE_NAME -f build/dockerfiles/Dockerfile.app .
+# Deploy Infrastructure for APP
+sam deploy \
+    --region us-west-2 \
+    --stack-name $APP_NAME \
+    --template-file template.yml \
+    --parameter-overrides ApiDomainName=$API_DOMAIN_NAME HostedZoneId=$HOSTED_ZONE_ID \
+    --guided
 
-# Tagging the docker image as the APP name.
-echo Tagging the Docker image...
-docker tag $ECR_SERVICE_NAME $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$ECR_SERVICE_NAME
-
-# Pusing the image to ECR repo created in APP infrastructure deployment.
-echo Pushing the Docker image...
-docker push $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/$ECR_SERVICE_NAME
-
-# Deploy new image to ECS Cluster for APP and SQS
-echo Deploying Image to ECS
-aws ecs --region us-west-2 update-service --cluster app-shared-infrastructure --service $APP_SERVICE_NAME --force-new-deployment > /dev/null &
-aws ecs --region us-west-2 update-service --cluster app-shared-infrastructure --service $SQS_SERVICE_NAME --force-new-deployment > /dev/null
+mv dependencies/nodejs/node_modules ./
